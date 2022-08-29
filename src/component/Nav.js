710d1cb1578326b739184e5cc4c8edd9102eca22
wen yo <wenda897436@gmail.com>
2022-08-29 14:31:51 +0800

    nav style

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,5 +1,13 @@
 function Nav(params) {
-    return <div className="w-[200px] grow-0 shrink-0 shadow-[1px_0_0_0_rgba(0,0,0,0.1)]">Nav</div>
+  return (
+    <div className="w-[200px] grow-0 shrink-0 shadow-[1px_0_0_0_rgba(0,0,0,0.1)] pt-12">
+      <ul className="text-sm">
+        <li className="px-5 py-2.5 cursor-pointer hover:text-sky-700">熱門報導</li>
+        <li className="px-5 py-2.5 cursor-pointer hover:text-sky-700">台灣</li>
+        <li className="px-5 py-2.5 cursor-pointer hover:text-sky-700">中國</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Nav;
