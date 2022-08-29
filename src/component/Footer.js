c0f36b866736c7e95fbf02f9a6f12fc53abc398a
wen yo <wenda897436@gmail.com>
2022-08-29 15:27:01 +0800

    footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,5 +1,14 @@
 function Footer(params) {
-    return <div className="h-[50px] w-full grow-0 shrink-0 shadow-[0_-1px_0_0_rgba(0,0,0,0.1)]">Footer</div>
+  return (
+    <ul className="h-[50px] w-full grow-0 shrink-0 flex items-center gap-2 px-4 text-gray-600 text-xs  shadow-[0_-1px_0_0_rgba(0,0,0,0.1)]">
+      <li>隱私權和 Cookie</li>
+      <li>法律聲明</li>
+      <li>廣告</li>
+      <li>關於我們的廣告</li>
+      <li>說明</li>
+      <li>意見反應</li>
+    </ul>
+  );
 }
 
 export default Footer;
