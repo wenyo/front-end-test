db231e6cd4c98c2bc4584e728242cc6eedd5e135
wen yo <wenda897436@gmail.com>
2022-08-29 14:07:16 +0800

    header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,12 @@
 function Header(params) {
   return (
-    <header className="h-[10vh] px-4 flex flex-row items-center justify-start ">
+    <header className="h-[12vh] px-4 flex flex-row items-center justify-start shadow-[0_1px_0_0_rgba(0,0,0,0.1)]">
       <h1 className="h-16">
         <img className="h-full" src="./Bing-logo.png" />
       </h1>
-      <input type="text" className="ml-4 rounded-full w-1/2 py-2 px-4 text-xs shadow-[2px_2px_8px_0_rgba(0,0,0,0.2)]" value="熱門報導"></input>
+      <div className="w-1/2 ml-4">
+        <input type="text" className=" rounded-full w-full py-2 px-4 text-xs shadow-[2px_2px_8px_0_rgba(0,0,0,0.2)]" value="熱門報導"></input>
+      </div>
     </header>
   );
 }
